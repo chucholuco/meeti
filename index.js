82a9f911e46fa8c723fa21ad712ad33e798ca447
Jesus Tapia <chucholuco@gmail.com>
2023-09-14 18:05:48 -0600

    adding routes and controllers

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
 const router = require('./routes')
-const routes = require('./routes')
+
+
 
 require('dotenv').config({path: 'variables.env'})
 
@@ -18,6 +19,13 @@ app.set('views', path.join(__dirname, './views'))
 
 // Archivos estaticos
 app.use(express.static('public'))
+
+// Middleware (usuario logueado, flash messages, fecha actual)
+app.use((req, res, next) => {
+    const fecha = new Date()
+    res.locals.year = fecha.getFullYear()
+    next()
+})
 
 //Routing
 app.use('/', router())
